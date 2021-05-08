fba34d6237df3dc591b61e85498072019dc48d21
noisestream <me@noisestream.de>
2021-05-08 12:10:38 +0200

    exponentielle Geschwindigkeit
     Wir haben die Steuerung für unser Auto so weit fertig. Mit den Pfeil-Tasten können wir vorwärts und rückwärts fahren und die Richtung ändern. Aber ...
    
        ... unser Auto fährt noch aus dem sichtbaren Bereich. Wie schaffen wir es, dies zu verhindern?
    
         ... die Beschleunigung ist nicht wirklich realistisch. Damit die Beschleunigung unseres Autos realistischer wird, soll sie sich exponentiell erhöhen. Das bedeutet, die Geschwindigkeit soll erst langsam schneller werden und zum Ende hin immer schneller. Natürlich brauchen wir dazu auch eine Höchstgeschwindigkeit.
    
        Pro: und wenn wir vom Gas gehen, soll unser Auto nicht einfach stoppen, sondern ausrollen.

--- pyg_noisecar.py
+++ pyg_noisecar.py
@@ -3,9 +3,15 @@ import math
 WIDTH = 1350
 HEIGHT = 629
 
-speed = 2
+speed = 0.2
+max_speed = 6
 
 car1 = Actor('noisecar1', (WIDTH/2, HEIGHT/2))
+
+prevPos = car1.pos
+
+carDOWN = False
+carUP = False
 
 def draw():
     screen.clear()
@@ -13,22 +19,54 @@ def draw():
 
 def update():
 
+    global prevPos, speed, carDOWN, carUP
+
     rad = math.radians(car1.angle)
 
     posx = math.sin(rad)
     posy = math.cos(rad)
 
-    print('X SIN: ' + str(posx) + 'Y COS: ' + str(posy))
+    print('Pos: ' + str(car1.pos))
+
+    if car1.x < WIDTH - 40 and car1.x > 40 and car1.y < HEIGHT - 40 and car1.y > 40:
+
+        prevPos = car1.pos
+
+        if keyboard.up:
+            if speed < max_speed:
+                speed = speed * 1.2
+            car1.y -= posy * speed
+            car1.x -= posx * speed
+            carUP = True
+            carDOWN = False
+        elif keyboard.down:
+            if speed < max_speed:
+                speed = speed * 1.2
+            car1.y += posy * speed
+            car1.x += posx * speed
+            carDOWN = True
+            carUP = False
+
+        else:
+            if speed > 0.2:
+                speed = speed * 0.98
+                if carUP:
+                    car1.y -= posy * speed
+                    car1.x -= posx * speed
+                elif carDOWN:
+                    car1.y += posy * speed
+                    car1.x += posx * speed
+    else:
+        car1.pos = prevPos
+
+    if speed > 0.2:
+        if keyboard.right:
+            car1.angle -= 1
+        if keyboard.left:
+            car1.angle += 1
 
 
 
-    if keyboard.up:
-        car1.y -= posy * speed
-        car1.x -= posx * speed
-    elif keyboard.down:
-        car1.y += posy * speed
-        car1.x += posx * speed
-    if keyboard.right:
-        car1.angle -= 1
-    if keyboard.left:
-        car1.angle += 1
+
+
+
